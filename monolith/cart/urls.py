7fced5bcfdcc86783f7d4e99d53cc60f4e1a92d8
AnhNQ08 <nguyenquanganh10082004@gmail.com>
2026-01-15 22:15:56 +0700

    delete all

--- monolith/cart/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import cart_detail
-
-urlpatterns = [
-    path('', cart_detail, name='cart_detail'),
-]
--- monolith/cart/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import CartItem
-
-def cart_detail(request):
-    items = CartItem.objects.all()
-    return render(request, 'cart/cart_detail.html', {'items': items})
